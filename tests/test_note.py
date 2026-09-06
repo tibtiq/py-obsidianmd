8d0d50d71c228e513ee0dd7df3d47f44dacf5455
tibtiq <29826331+tibtiq@users.noreply.github.com>
2026-09-06 04:24:25 +0000

    add test for Notes init

--- tests/test_note.py
+++ tests/test_note.py
@@ -3,7 +3,7 @@ from icecream import ic
 
 ic.configureOutput(includeContext=True)
 
-from pyomd.note import Note
+from pyomd.note import Note, Notes
 
 
 class TestNote:
@@ -12,3 +12,24 @@ class TestNote:
         temp_file.write_text("")
 
         Note(temp_file)
+
+
+class TestNotes:
+    def test_init_single(self, tmp_path):
+        temp_file = tmp_path / "note1.md"
+        temp_file.write_text("")
+
+        Note(temp_file)
+
+    def test_init_dir(self, tmp_path):
+        temp_file = tmp_path / "note1.md"
+        temp_file.write_text("")
+
+        notes = Notes(tmp_path)
+        assert len(notes) == 1
+
+        temp_file = tmp_path / "note2.md"
+        temp_file.write_text("")
+
+        notes = Notes(tmp_path)
+        assert len(notes) == 2
